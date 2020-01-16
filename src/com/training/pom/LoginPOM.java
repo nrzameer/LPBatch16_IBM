b76471f5acd7856af0fdc9c0b41c49934cb13f78
nrzameer <nrzameer@gmail.com>
2020-01-16 08:20:53 +0530

    Added common method

--- src/com/training/pom/LoginPOM.java
+++ src/com/training/pom/LoginPOM.java
@@ -5,8 +5,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import com.training.generics.GenericMethods;
+
 public class LoginPOM {
 	private WebDriver driver; 
+	
+	GenericMethods genericmethod;
 	
 	public LoginPOM(WebDriver driver) {
 		this.driver = driver; 
@@ -26,16 +30,20 @@ public class LoginPOM {
 	
 	
 	public void sendUserName(String userName) {
-		this.userName.clear();
-		this.userName.sendKeys(userName);
+		genericmethod = new GenericMethods(driver);
+		genericmethod.txtBoxVisibility(this.userName, userName);
+		//this.userName.clear();
+	//	this.userName.sendKeys(userName);
 	}
 	
 	public void sendPassword(String password) {
-		this.password.clear(); 
-		this.password.sendKeys(password); 
+		genericmethod.txtBoxVisibility(this.password, password);
+	//	this.password.clear(); 
+	//	this.password.sendKeys(password); 
 	}
 	
 	public void clickLoginBtn() {
-		this.loginBtn.click(); 
+		genericmethod.linkVisibility(this.loginBtn);
+	//	this.loginBtn.click(); 
 	}
 }
